e10c440ee129c5f17ee40fee68dfd423c50ee259
11xdullpo <11xdullpo@gmail.com>
2023-12-10 22:30:12 +0900

    2커밋 ㅋㅋㅋ

---
+++ src/main/java/com/company/test_1/web/dto/HelloResponseDto.java
@@ -0,0 +1,10 @@
+package com.company.test_1.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
--- src/test/java/com/company/test_1/web/HelloControllerTest.java
+++ src/test/java/com/company/test_1/web/HelloControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,7 +27,7 @@ public class HelloControllerTest {
 
     
     @Test
-    public void helloShouldReturnHello() throws Exception {
+    public void helloShouldReturn() throws Exception {
         String hello = "hello";
         // hello로 get요쳥을 함
         mvc.perform(get("/hello"))
@@ -33,5 +35,19 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 // 본문 내용을 검증
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDtoShouldReturn() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
